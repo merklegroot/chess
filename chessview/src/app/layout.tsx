5cbbe556e6a7e04fdabd4cde1722dc238d738f8a
Merkle Groot <merklegroot@gmail.com>
2025-05-10 00:37:16 -0400

    sticky menu stick to you!

--- chessview/src/app/layout.tsx
+++ chessview/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="bg-white shadow-sm">
+        <nav className="sticky top-0 bg-white shadow-sm z-50">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
               <div className="flex">
